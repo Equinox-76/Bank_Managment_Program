3c4b3bde0d935bcf44581eab2d6419e00667b496
Equinox-76 <redstonej318@gmail.com>
2024-10-05 22:08:51 -0400

    Removed comment

--- src/net/justin/Main.java
+++ src/net/justin/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // New comment
+
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<Bank> accounts = new ArrayList<>();
